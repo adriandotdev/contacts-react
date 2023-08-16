cb50156b0b75c25b0ca72ebbe4291b66d183cba0
Adrian Nads <marcelonads@gmail.com>
2023-08-17 07:06:39 +0800

    part-2: setup the store and provider at main.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createStore } from 'redux';
+import contactsReducer from './reducers/contactsReducer.js';
+import { Provider } from 'react-redux';
+
+const store = createStore(contactsReducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
