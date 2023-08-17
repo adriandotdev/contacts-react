2aac09adb29eaa4488ed7d13eaa24e8539b48220
Adrian Nads <marcelonads@gmail.com>
2023-08-17 23:16:34 +0800

    part-3: added  validation onChange event to form fields

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -78,15 +78,12 @@ function Form({ contactToUpdate, setContactToUpdate }) {
 
             firstNameFieldRef.current.focus();
         }
-        else {
-            alert("Please provide all of the fields")
-        }
     }
 
     const update = (e) => {
 
         e.preventDefault();
-        // console.log(e)
+
         if (isRequiredFieldsProvided()) {
 
             dispatch(updateContact(contact));
@@ -110,7 +107,7 @@ function Form({ contactToUpdate, setContactToUpdate }) {
                 lastName: true,
                 mobileNumber: true,
                 emailAddress: true
-            })
+            });
         }
         else
             setContact({
@@ -131,32 +128,72 @@ function Form({ contactToUpdate, setContactToUpdate }) {
                     setContact({ ...contact, firstName: e.target.value })
 
                     if (!e.target.value) {
-                        setFirstNameValid(false);
+                        setValidation({ ...validation, firstName: false });
                     }
                     else {
-                        setFirstNameValid(true);
+                        setValidation({ ...validation, firstName: true });
                     }
-                }} type="text" name="first-name" id="first-name" className={`form-control ${isFirstNameValid ? '' : 'is-invalid'}`} />
+                }} type="text" name="first-name" id="first-name" className={`form-control ${validation.firstName ? '' : 'is-invalid'}`} />
+                {!validation.firstName && <small className='text-danger fw-bold'>Please provide your first name</small>}
             </section>
 
-            <section>
+            <section className='mt-2'>
                 <label htmlFor="first-name" className="form-label">Middle Name</label>
-                <input value={contact.middleName} onChange={(e) => setContact({ ...contact, middleName: e.target.value })} type="text" name="middle-name" id="middle-name" className='form-control' />
+                <input value={contact.middleName} onChange={(e) => {
+                    setContact({ ...contact, middleName: e.target.value })
+
+                    if (!e.target.value) {
+                        setValidation({ ...validation, middleName: false });
+                    }
+                    else {
+                        setValidation({ ...validation, middleName: true });
+                    }
+                }} type="text" name="middle-name" id="middle-name" className={`form-control ${validation.middleName ? '' : 'is-invalid'}`} />
+                {!validation.middleName && <small className='text-danger fw-bold'>Please provide your middle name</small>}
             </section>
 
-            <section>
+            <section className='mt-2'>
                 <label htmlFor="first-name" className='form-label'>Last Name</label>
-                <input value={contact.lastName} onChange={(e) => setContact({ ...contact, lastName: e.target.value })} type="text" name="last-name" id="last-name" className='form-control' />
+                <input value={contact.lastName} onChange={(e) => {
+                    setContact({ ...contact, lastName: e.target.value });
+
+                    if (!e.target.value) {
+                        setValidation({ ...validation, lastName: false });
+                    }
+                    else {
+                        setValidation({ ...validation, lastName: true });
+                    }
+                }} type="text" name="last-name" id="last-name" className={`form-control ${validation.lastName ? '' : 'is-invalid'}`} />
+                {!validation.lastName && <small className='text-danger fw-bold'>Please provide your last name</small>}
             </section>
 
-            <section>
+            <section className='mt-2'>
                 <label htmlFor="first-name" className='form-label'>Mobile Number</label>
-                <input value={contact.mobileNumber} onChange={(e) => setContact({ ...contact, mobileNumber: e.target.value })} type="text" name="mobile-number" id="mobile-number" className='form-control' />
+                <input value={contact.mobileNumber} onChange={(e) => {
+                    setContact({ ...contact, mobileNumber: e.target.value });
+
+                    if (!MOBILE_PHONE_PATTERN.test(e.target.value))
+                        setValidation({ ...validation, mobileNumber: false });
+                    else
+                        setValidation({ ...validation, mobileNumber: true });
+
+                }} type="text" name="mobile-number" id="mobile-number" className={`form-control ${validation.mobileNumber ? '' : 'is-invalid'}`} />
+                {!validation.mobileNumber && <small className='text-danger fw-bold'>Please provide a valid phone number. (E.g. 09234451123) It must be start at 09, and follow by 9 digits.</small>}
             </section>
 
-            <section>
+            <section className='mt-2'>
                 <label htmlFor="first-name" className='form-label'>Email Address</label>
-                <input value={contact.emailAddress} onChange={(e) => setContact({ ...contact, emailAddress: e.target.value })} type="email" name="email" id="email" className='form-control' />
+                <input value={contact.emailAddress} onChange={(e) => {
+
+                    setContact({ ...contact, emailAddress: e.target.value });
+
+                    if (!EMAIL_PATTERN.test(e.target.value))
+                        setValidation({ ...validation, emailAddress: false });
+                    else
+                        setValidation({ ...validation, emailAddress: true });
+
+                }} type="email" name="email" id="email" className={`form-control ${validation.emailAddress ? '' : 'is-invalid'}`} />
+                {!validation.emailAddress && <small className='text-danger fw-bold'>Please provide a valid email. Accepted emails are email@gmail.com, and email@pragmanila.com</small>}
             </section>
 
             <input type="submit" value={`${contactToUpdate ? 'Update' : 'Save Contact'}`} className='btn btn-success mt-3 fw-bold' />
